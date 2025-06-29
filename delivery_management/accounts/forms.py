a5d730d248bf02c1dff0c443a188bdf1707cc55a
firasbarkia <firasbarkia090@gmail.com>
2025-06-29 19:36:40 +0100

    fixing

--- delivery_management/accounts/forms.py
+++ delivery_management/accounts/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser
+from .models import User
 
 class UserRegisterForm(UserCreationForm):
     class Meta:
-        model = CustomUser
-        fields = ['username', 'email', 'password1', 'password2', 'is_dispatcher', 'is_driver', 'is_manager']
+        model = User
+        fields = ['username', 'email', 'role']
--- delivery_management/delivery_management/urls.py
+++ delivery_management/delivery_management/urls.py
@@ -19,12 +19,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts import views as accounts_views
 
 from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('accounts/profile/', accounts_views.profile, name='profile'),
     path('deliveries/', include('deliveries.urls')),
+    path('dashboard/', include('dashboard.urls', namespace='dashboard')),
     path('', RedirectView.as_view(url='/deliveries/', permanent=True)),
 ]
